d6d265aa8fad030d2c82def7ab39622c23a0ccdb
pooria <bahremand@citynet.ir>
2024-09-09 12:30:16 +0330

    starting the blog branch

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
-  <div id="app">
-    <FormComponent>
-      <div slot="form-header">
-        <h3>This is the title of a form</h3>
-        <p>This is some info about the form</p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required />
-        <input type="password" placeholder="password" required />
-      </div>
-      <div slot="form-controls">
-        <button @click="handleSubmit">Submit</button>
-      </div>
-    </FormComponent>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import FormComponent from "./components/FormHelper.vue";
 export default {
-  components: {
-    FormComponent,
-  },
+  components: {},
 
   data() {
     return {};
   },
-  methods: {
-    handleSubmit: function () {
-      alert("thanks for submitting");
-    },
-  },
+  methods: {},
 };
 </script>
 
