7abce4c8a1bf674b301b5dae31457e7ba10812a1
pooria <bahremand@citynet.ir>
2024-09-09 12:26:02 +0330

    the first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <FormComponent>
+      <div slot="form-header">
+        <h3>This is the title of a form</h3>
+        <p>This is some info about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Submit</button>
+      </div>
+    </FormComponent>
+  </div>
+</template>
+
+<script>
+import FormComponent from "./components/FormHelper.vue";
+export default {
+  components: {
+    FormComponent,
+  },
+
+  data() {
+    return {};
+  },
+  methods: {
+    handleSubmit: function () {
+      alert("thanks for submitting");
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
+</style>
