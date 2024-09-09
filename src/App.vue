a205e4499886bf5f13e177b0c8b7c802ac65bed4
pooria <bahremand@citynet.ir>
2024-09-09 14:08:36 +0330

    2 way binding of check box

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <AddBlogComponent />
+  </div>
 </template>
 
 <script>
+import AddBlogComponent from "./components/AddBlog.vue";
+
 export default {
-  components: {},
+  components: {
+    AddBlogComponent,
+  },
 
   data() {
     return {};
